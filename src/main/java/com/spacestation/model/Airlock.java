62fe1c03b7ced3e1ab4e6611f9d3ad012aa40890
Mpilo Mnyandu <mpmnyswgc025@gmail.com>
2026-07-19 16:08:06 +0200

    Airlock class: added constructor and getters & setters

---
+++ src/main/java/com/spacestation/model/Airlock.java
@@ -0,0 +1,26 @@
+package main.java.com.spacestation.model;
+
+public class Airlock {
+    private String id;
+    private boolean isLocked; // true = locked, false = unlocked
+    
+    public Airlock(String id) {
+        this.id = id;
+        this.isLocked = true; // Start locked by default
+    }
+    
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+    
+    public boolean isLocked() { return isLocked; }
+    public void setLocked(boolean locked) { isLocked = locked; }
+    
+    // Helper methods (more meaningful)
+    public void lock() {
+        this.isLocked = true;
+    }
+    
+    public void unlock() {
+        this.isLocked = false;
+    }
+}
